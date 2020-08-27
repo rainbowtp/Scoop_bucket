82bfdb35fe0ed60b0efa97c4ab0d1666ecf64c89
Rainbow <1844283837@qq.com>
2020-08-27 20:56:17 +0800

    scoop完结

--- install.ps1
+++ install.ps1
@@ -1,1 +1,3 @@
-scoop install steam captura winrar cuda diskgenius everything-lite gamepp2 ZYplayer
+scoop install 7zip bandizip dark innounp aria2  mactype-np python  steam captura nvidia-display-driver-np cuda diskgenius everything-lite  sudo git vscode miniconda3 potplayer quicklook php mysql apache nodejs office-365-apps-np docker translucenttb windows-terminal posh-git  oh-my-posh ImageGlass sublime-text typora ffmpeg spacesniffer youtube-dl; 
+
+scoop install ZYplayer gamepp2 dota2mods wegame sougouinput wechat TIM utools MSIDragonCenter VM15
